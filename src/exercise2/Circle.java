88e51a4b2905e157a7c223b022a6238b6e5b73c6
mochkata <>
2021-10-20 17:45:46 +0000

    djamaikata

--- src/exercise2/Circle.java
+++ src/exercise2/Circle.java
@@ -3,18 +3,15 @@ package exercise2;
 import java.util.Scanner;
 
 public class Circle {
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-    
-        double r = input.nextDouble();
-        double c = 2 * Math.PI * r;
-        double a = Math.PI * r * r; 
+        System.out.println("Enter radius: ");
+        int radius=input.nextInt();
+        double circ=2*Math.PI*radius;
+        System.out.println("Circumference: "+ circ);
+        double ar=Math.PI*radius*radius;
+        System.out.println("Area: "+ ar);
+        }
 
-        System.out.println("Circumference:" + c);
-        System.out.println("Area:"+ a);
-          
     }
-
-}
